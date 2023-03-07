fe3506e65e76fdd2c4e478c9d15a063e8ad870d4
Azeroji <daliladimi33@gmail.com>
2023-03-07 21:38:54 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Card from './Card.js'
+
+export default function App() {
+    return(
+        <div style={{
+            display:"flex",
+            justifyContent:"center",
+            marginTop:"20vh"
+        }}>
+            <Card/>
+        </div>
+    )
+}
